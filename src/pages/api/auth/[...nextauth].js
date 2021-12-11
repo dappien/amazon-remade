a622aa4265878f04fa3276818a03cc1f03a96d5c
AllegoDev <93555187+AllegoDev@users.noreply.github.com>
2021-12-11 12:31:04 +0300

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.SECRET,
+      clientSecret: process.env.GOOGLE_SECRET,
   
     }),
     // ...add more providers here
