27bd0904e2e0479c12e0a5821e315fd7152c372a
AllegoDev <allego.dev@gmail.com>
2021-12-11 12:55:40 +0300

    Next Auth Updated

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,17 @@
 import NextAuth from "next-auth"
-import GoogleProvider from 'next-auth/providers/google';
-import React from 'react';
+import GoogleProvider from "next-auth/providers/google"
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.SECRET,
-  
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
- 
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
 })
